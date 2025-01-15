d5c2cf04846e1c313c91d360c2724fd08787f01f
vivianknee <vivianni452@gmail.com>
2025-01-14 16:41:46 -0800

    yuh

--- nav.js
+++ nav.js
@@ -5,9 +5,9 @@ class MyHeader extends HTMLElement{
         <nav>
             <ul>
                 <li><a class="main-tab" href="index.html">Vivian Ni</a></li>
-                <li><a class="tabs" href="campus-involvement.html" target="_blank" >Campus Involvement</a></li>
-                <li><a class="tabs" href="professional-development.html" target="_blank" >Professional Development</a></li>
-                <li><a class="tabs" href="projects.html" target="_blank" >Projects</a></li>
+                <li><a class="tabs" href="campus-involvement.html">Campus Involvement</a></li>
+                <li><a class="tabs" href="professional-development.html">Professional Development</a></li>
+                <li><a class="tabs" href="projects.html">Projects</a></li>
                 <li><a class="tabs" href="index.html">Home</a></li>
                 <button><a class="resume" href="Vivian Ni.pdf" target="_blank">Resume</a></button>
             </ul>
